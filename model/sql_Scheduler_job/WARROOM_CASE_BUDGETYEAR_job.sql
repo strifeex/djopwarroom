58db1f8fd75f9fcbcf6fbdb5048304523d6ea37d
strifeex <strifeex@gmail.com>
2022-07-07 11:22:09 +0700

    job schedule change get column and fixbug

--- model/sql_Scheduler_job/WARROOM_CASE_BUDGETYEAR_job.sql
+++ model/sql_Scheduler_job/WARROOM_CASE_BUDGETYEAR_job.sql
@@ -5,9 +5,9 @@ BEGIN
    job_action               =>  'BEGIN MERGE
 INTO    WARROOM_CASE_BUDGETYEAR_ALL d
 USING   (
-        	SELECT headYear, N, Y, NLL, N+Y+NLL AS TOTAL, SYSTIMESTAMP FROM(
+        	SELECT headYear, N, Y, K, N+Y+K AS TOTAL, SYSTIMESTAMP FROM(
 				SELECT * FROM (
-					SELECT to_char(sysdate, ''YYYY'')+543-1 AS headYear, JVN_ID, NVL2(CTRL_STATUS,CTRL_STATUS,0) AS stat FROM VDTL_CMST_CASE_MAIN_HD2 
+					SELECT to_char(sysdate, ''YYYY'')+543-1 AS headYear, JVN_ID, NVL2(CONTROL_STATUS,CONTROL_STATUS,0) AS stat FROM VDTL_CMST_CASE_MAIN_HD2 
 					WHERE RECEIVE_DATE BETWEEN to_date(''01-OCT-''||(to_char(sysdate, ''YY'')-1), ''DD-MONTH-YY'') 
 					AND to_date(TO_CHAR ((ADD_MONTHS (''01-OCT-''||(to_char(sysdate, ''YY'')-1),11)), ''YYYY-MM-DD'') || '' 23:59:59'', ''YYYY-MM-DD HH24:MI:SS'')
 				)
@@ -17,22 +17,23 @@ USING   (
 				    IN ( 
 				        ''N'' N,
 				        ''Y'' Y,
-				         0 NLL
+				        ''K'' K
 				    )
 				)
 			)
         ) s
-ON      (s.RECEIVE_YEAR = d.headYear)
+ON      (s.headYear = d.headYear)
 WHEN NOT MATCHED THEN
 INSERT (YEAR, ENSURE, CONTROL, NOCONTROL, TOTAL, UPDATE_TIME) 
+VALUES (headYear, N, Y, K,  N+Y+K, SYSTIMESTAMP) 
 WHEN MATCHED THEN 
 UPDATE 
 SET     ENSURE = N, 
         CONTROL = Y, 
-        NOCONTROL = NLL, 
+        NOCONTROL = K, 
         TOTAL = TOTAL, 
         UPDATE_TIME = SYSTIMESTAMP; END;', 
-    start_date				=>	to_date('15-JUN-2022 23:55:00','DD-MON-YYYY HH24:MI:SS'),
+    start_date				=>	to_date('06-JUL-2022 23:55:00','DD-MON-YYYY HH24:MI:SS'),
     repeat_interval			=>	'freq=daily;interval=1',
     enabled					=>	true
    );
--- model/sql_Scheduler_job/WARROOM_JVN_BUDGETYEAR_ALL_JOB.sql
+++ model/sql_Scheduler_job/WARROOM_JVN_BUDGETYEAR_ALL_JOB.sql
@@ -1,7 +1,7 @@
 BEGIN MERGE
 INTO    WARROOM_JVN_BUDGETYEAR_ALL d
 USING   (
-        	SELECT headYear, N, Y, NLL, N+Y+NLL AS TOTAL, SYSTIMESTAMP FROM(
+        	SELECT headYear, N, Y, K, N+Y+K AS TOTAL, SYSTIMESTAMP FROM(
 				SELECT * FROM (
 					SELECT DISTINCT to_char(sysdate, 'YYYY')+543-1 AS headYear, JVN_ID, NVL2(CTRL_STATUS,CTRL_STATUS,0) AS stat FROM VDTL_CMST_CASE_MAIN_HD2 
 					WHERE RECEIVE_DATE BETWEEN to_date('01-OCT-'||(to_char(sysdate, 'YY')-1), 'DD-MONTH-YY') 
@@ -13,18 +13,19 @@ USING   (
 				    IN ( 
 				        'N' N,
 				        'Y' Y,
-				         0 NLL
+				        'K' K
 				    )
 				)
 			)
         ) s
-ON      (s.RECEIVE_YEAR = d.headYear)
+ON      (s.headYear = d.headYear)
 WHEN NOT MATCHED THEN
 INSERT (YEAR, ENSURE, CONTROL, NOCONTROL, TOTAL, UPDATE_TIME) 
+VALUES (headYear, N, Y, K,  N+Y+K, SYSTIMESTAMP) 
 WHEN MATCHED THEN 
 UPDATE 
 SET     ENSURE = N, 
         CONTROL = Y, 
-        NOCONTROL = NLL, 
+        NOCONTROL = K, 
         TOTAL = TOTAL, 
         UPDATE_TIME = SYSTIMESTAMP; END;
